685a6d71a7608163462db39e51da1310d635efa1
SyukurGit <220705058@student.ar-raniry.ac.id>
2025-07-04 12:44:51 +0700

    Deskripsi perubahan

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
-// import { defineConfig } from 'vite'
-// import laravel from 'laravel-vite-plugin'
-// import tailwindcss from '@tailwindcss/vite'
-
-// export default defineConfig({
-//   plugins: [
-//     tailwindcss(),
-//     laravel({ input: ['resources/css/app.css', 'resources/js/app.js'], refresh: true }),
-//   ],
-// })
-
-
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // biar bisa diakses dari IP manapun
-    port: 5173,      // default Vite port
-    hmr: {
-      host: '192.168.154.223', // IP lokal kamu
-    },
-  },
   plugins: [
     tailwindcss(),
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
+    laravel({ input: ['resources/css/app.css', 'resources/js/app.js'], refresh: true }),
   ],
 })
+
+
+// import { defineConfig } from 'vite'
+// import laravel from 'laravel-vite-plugin'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   server: {
+//     host: '0.0.0.0', // biar bisa diakses dari IP manapun
+//     port: 5173,      // default Vite port
+//     hmr: {
+//       host: '192.168.154.223', // IP lokal kamu
+//     },
+//   },
+//   plugins: [
+//     tailwindcss(),
+//     laravel({
+//       input: ['resources/css/app.css', 'resources/js/app.js'],
+//       refresh: true,
+//     }),
+//   ],
+// })
