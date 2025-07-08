8174d8cb77e74a808ef6eeedb07b99d30f041b1d
SyukurGit <220705058@student.ar-raniry.ac.id>
2025-07-08 10:09:29 +0700

    initial commit to new repo

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LocalizationController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\DashboardController;
 
-// --- ROUTE PUBLIK 
+// --- ROUTE PUBLIK ---
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-
-// INI ROUTE YANG HILANG DAN PERLU DITAMBAHKAN KEMBALI
-// URL: /berita/{id} -> Nama Route: news.show
 Route::get('/berita/{news}', [DashboardController::class, 'show'])->name('news.show');
-
 
 // --- ROUTE AUTENTIKASI ---
 Route::controller(LoginController::class)->group(function () {
@@ -21,21 +16,13 @@ Route::controller(LoginController::class)->group(function () {
     Route::post('/logout', 'logout')->name('logout');
 });
 
-
 // --- ROUTE ADMIN YANG DILINDUNGI ---
 Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
-    // URL: /admin/input -> Nama Route: admin.input
     Route::get('/input', function () {
         return view('admininput');
     })->name('input');
 
-    // URL: /admin/news -> Nama Route: admin.news.index
     Route::get('/news', [NewsController::class, 'index'])->name('news.index');
-
-    // URL: /admin/news/store -> Nama Route: admin.news.store
     Route::post('/news/store', [NewsController::class, 'store'])->name('news.store');
-
-    // URL: /admin/news/{id}/delete -> Nama Route: admin.news.destroy
     Route::post('/news/{news}/delete', [NewsController::class, 'destroy'])->name('news.destroy');
 });
-
